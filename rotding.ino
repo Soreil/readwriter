0b2519c82895c31bb375e4ac2971718f8e2d6e17
Sjon Kosse <sjon@x61.eu>
2017-03-19 23:52:29 +0100

    Cleaned up features not used in final working version and removed dead code.

--- rotding.ino
+++ rotding.ino
@@ -14,17 +14,14 @@ const int dip[4] = { 12,8,7,6 };
 const unsigned long syncTime = 15; //ms, ulong is same as Arduino libraries use for time.
 const unsigned long bitTime = 9; //ms
 
+//Change readStart and writeStart for different Arduino's as the program is compiled and uploaded.
+
 //Bit on which we start reading
 const int readStart = 92;
 //Bit on which we start writing
 const int writeStart = 4;
 //Amount of bits to read and write
 const int messageLength = 4;
-//Wheter or not we are working in tandem with another group, @INCOMPLETE
-const bool groupversion = false;
-
-//Character read in from serial console.
-char ourChar = '\0';
 
 //Makes a non hardware interrupt pin behave as interrupt
 void pciSetup(uint8_t pin)
@@ -48,9 +45,6 @@ void setup() {
 	pinMode(dip[3], INPUT_PULLUP);
 
 	pciSetup(in);
-	Serial.begin(115200);
-	//Read character for lab 3
-	Serial.readBytes(&ourChar, 1);
 }
 
 //Time of last fired ISR.
@@ -77,15 +71,9 @@ ISR(PCINT1_vect) {
 
 	//We are in our range of bits to read luckily
 	if (edgeCount/2 >= readStart && edgeCount/2 < readStart + messageLength) {
-		//Serial.print("Reading wire");
-		//Serial.println(edgeCount);
 		if (digitalRead(in) == LOW) {
 			//Write to the subindex of the read bit where the LED is
 			digitalWrite(led[edgeCount/2 - readStart], !((NewTime - time) > 4));
-			//Serial.print("Current edge:");
-			//Serial.println(edgeCount / 2 - readStart);
-			//Serial.print("State:");
-			//Serial.println((NewTime - time) > 4);
 			edgeCount++;
 			time = NewTime;
 			return;
@@ -94,25 +82,14 @@ ISR(PCINT1_vect) {
 
 	//We are in our write range
 	if (edgeCount/2 >= writeStart && edgeCount/2 < writeStart + messageLength) {
-		//Serial.print("Writing wire");
-		//Serial.println(edgeCount);
 		if (digitalRead(in) == HIGH) {
 			//If the dip isn't high we really don't care for writing on the wire.
-			Serial.print("Interested in state of digital");
-			Serial.println(edgeCount / 2 - writeStart);
 			if (digitalRead(dip[edgeCount/2 - writeStart]) == LOW) {
 				unsigned long writeDuration = NewTime;
-				//while (millis() - writeDuration < (bitTime / 4) * 3) {
-				//	Serial.print("Line start value:");
-				//	Serial.println(digitalRead(out));
-				//	digitalWrite(out, digitalRead(dip[edgeCount / 2 - writeStart]));
-				//	Serial.print("Line end value:");
-				//	Serial.println(digitalRead(out));
-				//}
 				digitalWrite(out, HIGH);
 				//This delay is very much not the tool we want but interrupts can fire during a delay so it's less awful than it looks.
 				//We aren't very interested in general in interrupts that fire during our write moment.
-				delay(6);
+				delay((bitTime/4)*3);
 			}
 			digitalWrite(out, LOW);
 			edgeCount++;
